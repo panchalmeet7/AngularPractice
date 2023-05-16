e8eac1f8fab6cf883ce6ac15c9a34059d84f9ade
meet <meetpanchal194@gmail.com>
2023-05-16 18:26:47 +0530

    two way data binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,4 +5,29 @@ import {Component} from '@angular/core';
 })
 
 export class ServerComponent{
+    serverId = 7;
+    serverStatus = "online";
+    allowserverstatus = false;
+    username = 'meet';
+
+    // constructor()
+    // {
+    // setTimeout(() => {
+    //     this.allowserverstatus = true;
+       
+    // }, 2000);
+    // }
+
+    clickevent(){
+        console.log("function call");
+        //var meets =  this.serverId = 7? '19': '563485';
+        this.serverStatus = "online" ? this.serverStatus = "offline" : this.serverStatus = "online"
+        //console.log(this.serverStatus);
+        
+    }
+
+    changename(event: any){
+       this.username = event.target.value;
+        
+    }
 }
